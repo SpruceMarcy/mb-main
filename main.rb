ec3a638df69de6a69a493c42a4349e3bc27d7ea7
SpruceMarcy <mdr.brook@gmail.com>
2019-03-29 18:35:09 +0000

    Login tests

--- main.rb
+++ main.rb
@@ -1,5 +1,35 @@
+require 'net/smtp'
+require 'omniauth'
 require 'sinatra'
     set :bind, '0.0.0.0'
+
+password=''
+config = {
+    :consumer_key => "2479916b-8035-4981-b2e2-3b48b883ebe8",
+    :consumer_secret => "d!?>O|6$_};&})**{{>.>Z;#.^/}^&^^(={;?!*})^_?^/$)J{({}.",
+    }
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :azure, config[:consumer_key], config[:consumer_secret]
+end
+
+enable :sessions
+set :session_secret, 'key goes here'
+
+get "/login" do
+    erb :login
+end
+post '/auth/:name/callback' do
+    auth = request.env['omniauth.auth']
+    @authentication = Authentication.find_with_omniauth(auth)
+    if @authentication.nil?
+      @authentication = Authentication.create_with_omniauth(auth)
+    end 
+end
+
+
+
+
+
 
 get "/" do
     erb :index
@@ -19,6 +49,31 @@ end
 get "/about" do
     erb :about
 end
+get "/tools/settings" do
+    erb :settings
+end
+
+post "/tools/settings/admin" do
+   if !params[:password].nil?
+       password=params[:password] 
+   end
+   redirect "/tools/"
+end
+post "/tools/settings/session" do
+   session[:debug]=!params[:debug].nil?
+   redirect "/tools/"
+end
+
 get "/contact" do
     erb :contact
+end
+post "/contact" do
+    message = params[:message]
+    msg="From: MB-Main <mdr.brook@gmail.com>\nTo: Marcy Brook <mdr.brook@gmail.com>\nSubject: A Message from a Site Visitor.\n\n" + message + "\n\n"
+    smtp = Net::SMTP.new 'smtp.gmail.com', 587
+    smtp.enable_starttls
+    smtp.start('localhost', "mdr.brook@gmail.com", password, :login) do
+        smtp.send_message(msg, '', "mdr.brook@gmail.com")
+    end
+    redirect "/"
 end
