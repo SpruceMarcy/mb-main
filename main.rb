11d535ee5d696f83fdeedc3a96955e04cf6413e5
SpruceMarcy <mdr.brook@gmail.com>
2019-03-31 12:54:00 +0000

    the return of the azure

--- main.rb
+++ main.rb
@@ -14,7 +14,7 @@ use OmniAuth::Builder do
   provider :microsoft_live, 
     {client_id: config[:consumer_key],
         client_secret: config[:consumer_secret],
-        redirect_uri: "https://mb-main-test.herokuapp.com/",
+        redirect_uri: "https://mb-main-test.herokuapp.com/auth/azure_oauth2/callback",
       #tenant_id: ENV['AZURE_TENANT_ID']
     }
 end
--- vendor/gems/omniauth-microsoft-live/lib/omniauth/strategies/microsoft_live.rb
+++ vendor/gems/omniauth-microsoft-live/lib/omniauth/strategies/microsoft_live.rb
@@ -10,7 +10,7 @@ module OmniAuth
         :site => 'https://login.live.com',
         :authorize_url => '/oauth20_authorize.srf',
         :token_url => '/oauth20_token.srf',
-        :redirect_uri => 'https://mb-main-test.herokuapp.com/'
+        :redirect_uri => 'https://mb-main-test.herokuapp.com/auth/azure_oauth2/callback'
       }
 
       option :authorize_params, {
