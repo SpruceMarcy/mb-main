794ca9d972635a54b8bd0cd155a08996d09f4c4f
SpruceMarcy <mdr.brook@gmail.com>
2019-07-14 01:05:10 +0000

    overhauled css comparer

--- public/javascripts/cssstreamline.js
+++ public/javascripts/cssstreamline.js
@@ -53,29 +53,86 @@ function cssObjectToText(cssOb){
     }
     return text
 }
-function intersectionOfCssObjects(cssOb1,cssOb2){
+function compareCssObjects(cssOb1,cssOb2){
+    var c1={}
     var intersect={}
     for(var selector1 in cssOb1){
-        for(var selector2 in cssOb2){
-            if(selector1==selector2){
-                for(var rule1 in cssOb1[selector1]){
-                    for(var rule2 in cssOb2[selector2]){
-                        if(cssOb1[selector1][rule1]==cssOb2[selector2][rule2]){
-                            if(typeof intersect[selector1] == 'undefined'){
-                                intersect[selector1]=[]
-                                //wow, I unironically got to 8 indents. nice.
-                            }
-                            intersect[selector1].push(cssOb1[selector1][rule1])  
-                        }
+        if(typeof cssOb2[selector1] != 'undefined'){
+            for(var rule1 in cssOb1[selector1]){
+                var isUnique=true
+                for(var rule2 in cssOb2[selector1]){
+                    if(cssOb1[selector1][rule1]==cssOb2[selector1][rule2]){
+                        addRuleToCssObject(intersect,selector1,cssOb1[selector1][rule1])
+                        isUnique=false
                     }
+                }
+                if(isUnique){
+                    addRuleToCssObject(c1,selector1,cssOb1[selector1][rule1])
                 }
             }
         }
+        else{
+            for(var rule1 in cssOb1[selector1]){
+                addRuleToCssObject(c1,selector1,cssOb1[selector1][rule1])
+            }
+        }
     }
-    return intersect
+    return [c1,intersect]
+}
+function addRuleToCssObject(cssOb, selector, rule){
+    if(typeof cssOb[selector] == 'undefined'){
+        cssOb[selector]=[]
+    }
+    cssOb[selector].push(rule)  
+}
+function subtractCssObjects(cssOb1,cssOb2){
+    var c1={}
+    for(var selector1 in cssOb1){
+        if(typeof cssOb2[selector1] != 'undefined'){
+            for(var rule1 in cssOb1[selector1]){
+                var isUnique=true
+                for(var rule2 in cssOb2[selector1]){
+                    if(cssOb1[selector1][rule1]==cssOb2[selector1][rule2]){
+                        isUnique=false
+                    }
+                }
+                if(isUnique){
+                    addRuleToCssObject(c1,selector1,cssOb1[selector1][rule1])
+                }
+            }
+        }
+        else{
+            for(var rule1 in cssOb1[selector1]){
+                addRuleToCssObject(c1,selector1,cssOb1[selector1][rule1])
+            }
+        }
+    }
+    return c1
 }
 document.getElementById("compare").addEventListener("click",function(){
-    css1=textToCssObject(document.getElementById("input1").value)
-    css2=textToCssObject(document.getElementById("input2").value)
-    document.getElementById("output").value=cssObjectToText(intersectionOfCssObjects(css1,css2))
+    output=""
+    try{
+        css1=textToCssObject(document.getElementById("input1").value)
+        css2=textToCssObject(document.getElementById("input2").value)
+        comparison=compareCssObjects(css1,css2)
+        if(document.getElementById("c1").checked){
+            output=cssObjectToText(comparison[0])
+        }
+        if(document.getElementById("intersection").checked){
+            output=cssObjectToText(textToCssObject(output+cssObjectToText(comparison[1])))
+        }
+        if(document.getElementById("c2").checked){
+            output=cssObjectToText(textToCssObject(output+cssObjectToText(subtractCssObjects(css2,comparison[1]))))
+        }
+        if(output==""){
+            output="No rules found"
+        }
+        if(!(document.getElementById("c1").checked||document.getElementById("intersection").checked||document.getElementById("c2").checked)){
+            output="No selection made"
+        }
+    }
+    catch(err){
+        output=err+"\n\nPlease report this error at http://mxmbrook.co.uk/contact along with a brief description of what happened when it occurred."
+    }
+    document.getElementById("output").value=output
 })
