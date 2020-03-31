0af43ba6f997bf28c33519b2c1927221a20b4a46
ach17mb <mbrook1@sheffield.ac.uk>
2020-03-31 18:27:29 +0100

    first seasonal style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
+require 'date'
 class ApplicationController < ActionController::Base
   skip_forgery_protection
+  before_action :before
   adminuid=ENV["master_name"]
+
+
+  private
+
+  def before
+    today=Date.today
+    if today==Date.new(2020,3,31)
+      @seasonal_style="tdov"
+      @seasonal_text="It's Transgender Day of Visibility 2020!"
+    end
+  end
+
   def query(q)
     conndomain=ENV["data_dom"]
     connport=ENV["data_port"].to_i
