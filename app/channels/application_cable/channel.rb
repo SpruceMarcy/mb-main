32ae2ececc54b5370cbc55c4fb218e498cda9be7
ach17mb <mbrook1@sheffield.ac.uk>
2020-04-03 22:30:57 +0100

    attempt to fix heroku crash

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,10 +1,4 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
-    def subscribed
-      stream_from "channel"
-    end
-    def recieve(data)
-      ActionCable.server.broadcast("channel",message: "YOLO")
-    end
   end
 end
