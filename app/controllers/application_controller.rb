f8d2660df49cf69596a5a92a648726b1f123a5c0
SpruceMarcy <mbrook1@sheffield.ac.uk>
2020-04-17 00:23:55 +0100

    more style changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   def before
     adminuid=ENV["master_name"]
     puts "DEBUG               !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    @isadmin=adminuid==session[:name]
+    @isadmin=adminuid==session[:uid]
     @adminuid=adminuid
     @includerecaptcha=false
     today=Date.today
