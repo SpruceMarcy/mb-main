f02d3af42afaced3d1523e6ef355f8883d187e1b
SpruceMarcy <mbrook1@sheffield.ac.uk>
2020-04-19 13:38:12 +0100

    try again

--- db/migrate/20200403143340_create_messages.rb
+++ db/migrate/20200403143340_create_messages.rb
@@ -1,6 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
+      t.integer :id, :primary => true
       t.text :content
 
       t.timestamps
--- db/migrate/20200419120518_add_id_to_messages.rb
+++
@@ -1,5 +0,0 @@
-class AddIdToMessages < ActiveRecord::Migration[6.0]
-  def change
-    add_column :messages, :id, :primary_key
-  end
-end
